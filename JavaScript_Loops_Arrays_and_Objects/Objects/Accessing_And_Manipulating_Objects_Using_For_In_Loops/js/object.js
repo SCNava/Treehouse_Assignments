5a2f7725028a162a66d67301b79650c5b32b525f
unknown <steven.nava@trojans.dsu.edu>
2019-06-02 00:20:10 -0400

    Added tutorial for accessing and manipulating object property names and property values using for in loops

---
+++ JavaScript_Loops_Arrays_and_Objects/Objects/Accessing_And_Manipulating_Objects_Using_For_In_Loops/js/object.js
@@ -0,0 +1,37 @@
+function print(message) {
+  var div = document.getElementById('output');
+  div.innerHTML = message;
+}
+
+function printAppend (message) {
+  var div = document.getElementById('output');
+  div.innerHTML += message;
+}
+
+var person = {
+  name : 'Sarah',
+  country : 'US',
+  age : 35,
+  treehouseStudent : true,
+  skills : ['JavaScript', 'HTML', 'CSS']
+};
+
+// All object properties can utilize the properties and methods of their type
+
+
+var message = '<p>Hello. My name is ' + person.name + '</p>';
+message += '<p>I live in the ' + person.country + '</p>';
+person.name = 'Rainbow Dash';
+message += '<p>But, I wish my name was ' + person.name + '</p>';
+person.age += 1;
+message += '<p>My age is now ' + person.age + '</p>';
+message += '<p>I have ' + person.skills.length + ' skills: ';
+message += person.skills.join(', ') + '</p>';
+
+print(message);
+
+// For In Loop to Access Object Properties
+for ( property in person ) {
+  // In a for in loop you can *only* access the properties inside of the object using square bracket notation
+  printAppend('<p>' + property + ': ' + person[property] + '</p>');
+}
